abd4f9448b7a5629aa1e083bae1c21b3733a3367
Disastris <chavez.garcia.santi@gmail.com>
2023-10-30 11:00:11 -0500

    Added some more stuff on the schema and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-
+USE employee_db;
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 department_name VARCHAR(30) NOT NULL UNIQUE
@@ -18,5 +18,5 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT,
+manager_id INT
 );
--- server.js
+++ server.js
@@ -5,7 +5,47 @@ const inquirer =require('inquirer')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// mysql -u root - p
+// <password>
+// Equivalent:
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '512Green',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the database.`)
+);
+  
+function queryAllDepartments() {
+    const sql = `SELECT * from department`;
+  
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log("Error: There's a problem running the query")
+            return;
+        }
+        console.table(result)
+    });
+}
+function queryInsertDepartment(departmentName) {
+    const sql = `INSERT INTO department (department_name) VALUES (${departmentName})`;
+  
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log("Error: There's a problem running the query")
+            return;
+        }
+        console.table(result)
+    });
+}
 
+// For example, if inquirer has an option to View All Departments, and the user choses it
+// Then you'd call this function:
+queryAllDepartments()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
