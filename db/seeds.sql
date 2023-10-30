c7e9022d1728d09486a50243767c97f06961590e
Disastris <chavez.garcia.santi@gmail.com>
2023-10-30 17:26:00 -0500

    Added some more stuff to the seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,12 @@ INSERT INTO department (department_name)
 VALUES ("Sales"),
 ("Engineering"),
 ("Finance"),
-("Legal")
+("Legal");
+
+INSERT INTO role (department_id, title, salary)
+VALUES (1, "Salesperson", 80000),
+(2, "Softwear Engineer", 120000),
+(3, "Accountant", 125000),
+(4, "Attorney", 190000),
+(2, "Lead Engineer", 150000),
+(4, "Legal Team Lead", 250000);
